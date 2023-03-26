d7953f85998db377a1f80790f6feed0be6393ef0
vgulbickaja <106967101+vgulbickaja@users.noreply.github.com>
2023-03-26 22:17:57 +0300

    getting coin info via Axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    console.log("I work");
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cryptoHead"></div>
+      <div className="cryptoList"></div>
     </div>
   );
 }
