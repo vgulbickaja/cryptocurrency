5488af7ca20f34499c6ee2ca60cb596c5c5d289f
vgulbickaja <106967101+vgulbickaja@users.noreply.github.com>
2023-03-27 12:56:57 +0300

    css to display indiv coins in boxes

--- src/Components/Coin.jsx
+++ src/Components/Coin.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Coin({ name, price, symbol, icon }) {
   return (
     <div className="coin">
-      <h2>Name:{name}</h2>
-      <h3>Price:{price}</h3>
+      <h2>{name}</h2>
+      <h3>Price: {price}</h3>
       <h3>Symbol: {symbol}</h3>
       <img src={icon} />
     </div>
