2934c2a0f4d42ced9546f3c591ae5ebcabf85983
vgulbickaja <106967101+vgulbickaja@users.noreply.github.com>
2023-04-05 12:03:17 +0300

    style changed, added background

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
-import "./App.css";
-import Axios from "axios";
+import React from "react";
 import { useEffect, useState } from "react";
+import Axios from "axios";
 import Coin from "./Components/Coin";
+import "./App.css";
+import background from "./Assets/background.jpg";
 
 function App() {
   const [listOfCoins, setListOfCoins] = useState([]);
-  const [wordSearch, setWordSearch] = useState("");
-
-  const filtering = listOfCoins.filter((coin) => {
-    return coin.name.toLowerCase().includes(wordSearch.toLowerCase());
-  });
+  const [searchCrypto, setSearchCrypto] = useState("");
 
   useEffect(() => {
     Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
       (response) => {
         setListOfCoins(response.data.coins);
-        //console.log(response.data);
+        console.log(response.data);
       }
     );
   }, []);
+
+  const filtered = listOfCoins.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchCrypto.toLowerCase());
+  });
+
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
       <div className="cryptoHeader">
         <input
           type="text"
-          placeholder="Enter currency name..."
+          placeholder="type in cryptocurrency..."
           onChange={(event) => {
-            setWordSearch(event.target.value);
+            setSearchCrypto(event.target.value);
           }}
         />
       </div>
       <div className="cryptoDisplay">
-        {filtering.map((coin) => {
+        {filtered.map((coin) => {
           return (
             <Coin
               name={coin.name}
               price={coin.price}
-              symbol={coin.symbol}
               icon={coin.icon}
+              symbol={coin.symbol}
             />
           );
         })}
--- src/Components/Coin.jsx
+++ src/Components/Coin.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Coin({ name, price, symbol, icon }) {
+function Coin({ name, price, icon, symbol }) {
   return (
     <div className="coin">
-      <h1>{name}</h1>
-      <p>Price: {price}</p>
-      <p>Symbol: {symbol}</p>
+      <h3>{name}</h3>
+      <h3>Price: {price}</h3>
       <img src={icon} />
+      <h3>Symbol: {symbol} </h3>
     </div>
   );
 }
